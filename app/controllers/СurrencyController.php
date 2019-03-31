5c672a121f4c09cf9ff0d6c1effc109726717bbb
Manchu Dmitrii <manchu.dmitrii@gmai.com>
2019-03-31 18:22:59 +0300

    Currency Composer + MVS Starter

---
+++ app/controllers/СurrencyController.php
@@ -0,0 +1,7 @@
+<?php
+    namespace App\Controllers;
+    class СurrencyController{
+        function __construct(){
+            echo 'CC Controller';
+        }
+    }
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  require_once __DIR__ . '/vendor/autoload.php';
+
+  
+   $СurrencyController = new App\Controllers\СurrencyController();
+   $СurrencyModel = new App\Models\СurrencyModel();
